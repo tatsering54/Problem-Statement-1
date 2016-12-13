fd80b2c2e55fca545ec0674d0e577401aaeb90ea
tashi tsering <tatsering54@gmail.com>
2016-12-13 17:54:51 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,12 @@
+class example
+{
+ int x; int y;
+ String name;
+ public static void main(String args[]) 
+	{
+ 		example pnt = new example();
+ 		System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+	}
+}
+
+// Anwser is d.the program prints pnt is null 0 0.
